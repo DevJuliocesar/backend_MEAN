423e5ceffec2abdf9cbb675d71a0e3e69eec2170
Julio Cesar Maldonado Moreno <jcmaldonadom@ufpso.edu.co>
2019-03-23 16:17:51 -0500

    actualizando

--- app.js
+++ app.js
@@ -4,22 +4,22 @@ var mongoose = require('mongoose');
 var app = express();
 
 // Conexión base de datos
-mongoose.connect('mongodb://localhost/hospitalDB');
+mongoose.connect('mongodb://localhost/hospitalDB', { useNewUrlParser: true });
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-    console.log("we're conected!")
-    // we're connected!
+db.once('open', function() {
+  console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
+  // we're connected!
 });
 
 app.get('/', (req, res, next) => {
-    res.status(403).json({
-        ok: true,
-        mensaje: 'Petición realizada correctamente'
-    });
+  res.status(403).json({
+    ok: true,
+    mensaje: 'Petición realizada correctamente'
+  });
 });
 
 app.listen(3000, () => {
-    console.log('Express server puerto 3000 online');
+  console.log('Express server puerto 3000 online');
 });
